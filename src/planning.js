34450746e6e41044aad25cb7c3a5eb86ceb71125
cap1O <18724661+capi1O@users.noreply.github.com>
2026-01-07 22:46:35 +0100

    move files in src dir

--- src/planning.js
+++ src/planning.js
@@ -3,8 +3,8 @@
 // @match        https://www.notion.so/*
 // ==/UserScript==
 
-import { addDayJumpButtons } from "../notion-day-jump-buttons.js";
-import { addSidePeekButtons } from "../notion-side-peek-buttons.js";
+import { addDayJumpButtons } from "./notion-day-jump-buttons.js";
+import { addSidePeekButtons } from "./notion-side-peek-buttons.js";
 
 addDayJumpButtons();
 addSidePeekButtons();
