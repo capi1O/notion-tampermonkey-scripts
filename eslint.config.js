5e668583ef39635ce0465bf2877643ca461f48f1
cap1O <18724661+capi1O@users.noreply.github.com>
2026-01-08 08:53:52 +0100

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+	{
+		files: ['**/*.{js,mjs,cjs}'],
+		plugins: {
+			js,
+		},
+		extends: ['js/recommended'],
+		languageOptions: {
+			globals: globals.browser,
+		},
+		rules: {
+			'indent': ['error', 'tab'],
+			'no-tabs': 'off',
+			'quotes': ['error', 'single'],
+			'prefer-arrow-callback': ['error'],
+  	  'func-style': ['error', 'expression'],
+			'semi': ['error', 'always'],
+			'comma-dangle': ['error', 'always-multiline'],
+		},
+		ignores: [],
+	},
+	{
+		files: ['**/*.json'],
+		plugins: {
+			json,
+		},
+		language: 'json/json',
+		extends: ['json/recommended'],
+	},
+	{
+		files: ['**/*.md'],
+		plugins: {
+			markdown,
+		},
+		language: 'markdown/gfm',
+		extends: ['markdown/recommended'],
+	},
+]);
